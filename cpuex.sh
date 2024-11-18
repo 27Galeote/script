c386c3e02325844d04da34e9693520f7e5a6a961
27Galeote <jgaleotec@gmail.com>
2024-11-18 20:43:10 +0100

    update

---
+++ cpuex.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+PID=$1
+
+DADES=$(ps -p $PID -o cputime=, etime=)
+
+CPUTIME=$(echo "$DADES" | cut -d ' ' -f 1)
+ETIME=$(echo "$DADES" | cut -d ' ' -f 2)
+
+CPUTIME_SEC=$(echo $CPUTIME | cut -d ':' --output-delimiter=' ' -f 1,2,3 | { read h m s; echo $((10#$h*3600 + 10#$m*60 + 10#$s)); })
+ETIME_SEC=$(echo $ETIME | cut -d ':' --output-delimiter=' ' -f 1,2,3 | { read h m s; echo $((10#$h*3600 + 10#$m*60 + 10#$s));})
+
+CPU_PERCENT=$(( 100 * CPUTIME_SEC / ETIME_SEC ))
+
+echo "$CPU_PERCENT% de CPU utilitzat pel procés amb PID $PID"
